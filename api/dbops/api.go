2db5c62fe0c31342d7ab676857c198c24604a90a
GoGoCoding <it_haoqi@163.com>
2020-06-15 14:04:06 +0800

    init

---
+++ api/dbops/api.go
@@ -0,0 +1,15 @@
+package dbops
+
+import "database/sql"
+
+func openConn() *sql.DB {
+
+}
+
+func AddUserCredentail(loginName string, pwd string) error {
+
+}
+
+func GetUserCredentail(loginName string) (string, error) {
+
+}
